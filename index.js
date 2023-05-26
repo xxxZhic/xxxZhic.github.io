1609a510084d07ed92533e9280be327dd113872c
huzhicheng <hzclala@gmail.com>
2023-05-26 16:53:50 +0800

    test

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-window.onload = function() {
-    document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
-  }
+window.addEventListener('DOMContentLoaded', (event) => {
+    // 在DOM加载完成后执行的代码
+    
+    // 通过JavaScript添加照片和视频到相册
+    
+    // 示例代码：
+    
+    // 获取相册容器
+    const gallery = document.getElementById('gallery');
+    
+    // 创建照片元素
+    const photo = document.createElement('img');
+    photo.src = 'path_to_your_photo.jpg';
+    photo.alt = '旅游照片';
+    
+    // 创建视频元素
+    const video = document.createElement('video');
+    video.src = 'path_to_your_video.mp4';
+    video.controls = true;
+    
+    // 将照片和视频添加到相册容器
+    gallery.appendChild(photo);
+    gallery.appendChild(video);
+  });
+  
   
