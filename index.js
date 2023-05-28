acf9fc40461d6f060751f031366dce46be1590a9
huzhicheng <hzclala@gmail.com>
2023-05-28 22:19:30 +0800

    hi

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    // 在DOM加载完成后执行的代码
-    
-    // 通过JavaScript添加照片和视频到相册
-    
-    // 示例代码：
-    
-    // 获取相册容器
+
     const gallery = document.getElementById('gallery');
     
-    // 创建照片元素
+  
     const photo = document.createElement('img');
     photo.src = 'photos/IMG_4787.jpeg';
     photo.alt = 'photos';
     
-    // 创建视频元素
+
     const video = document.createElement('video');
     video.src = 'path_to_your_video.mp4';
     video.controls = true;
     
-    // 将照片和视频添加到相册容器
+
     gallery.appendChild(photo);
     gallery.appendChild(video);
   });
